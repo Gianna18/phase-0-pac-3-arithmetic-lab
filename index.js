31f04d26dacdbd523f429034211c1c929b4b7668
Markmutuku <mark.mutuku@student.moringaschool.com>
2022-11-21 12:29:03 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,36 @@
+function add() {
+    1 + 80;
+}
+function subtract() {
+    50 - 30;
+}
+function multiply() {
+    3 * 1.2;
+}  
+function divide() {
+    75 / 5;
+}
+function add(a, b) {
+ return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+function increment(n) {
+    return n +1;
+}
+function decrement(n) {
+    return n -1;
+}
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+function preserveDecimal(n) {
+    return parseFloat(n, 10);
+}
